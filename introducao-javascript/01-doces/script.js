e7b9d41c5dc7d502cc621ea19cb548d8c0de6648
LucasRiberio7 <lucasribeiro.222011@gmail.com>
2026-08-11 11:42:55 -0300

    Javascript-aula-1-

---
+++ introducao-javascript/01-doces/script.js
@@ -0,0 +1,11 @@
+//comentário do javascript usamos duas barras
+//criar variaveis
+//As variaveis guardam um valor de um tipo diferentes
+
+const nomeDoce ="pé de moleque"; //sping(texto)
+const preco =2.10;// preço do pudim 
+const disponivel =true;//true or false
+
+console.log(nomeDoce,preco,disponivel);//para isso realmente funcionar
+document.getElementById(resultado).textContent;
+`${nomeDoce} custa R$ ${preco.toFixed(2)} - disponível: ${disponivel ? "disponivel": "indisponivel"}`;
